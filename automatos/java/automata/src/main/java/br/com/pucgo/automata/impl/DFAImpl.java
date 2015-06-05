5c2ffda86a9a0e0fc8e5d1fb3eaad010bc59d3b0
Antony Alkmim <antonyalkmim@MAC-ANTONY.local>
2015-06-05 09:50:31 -0300

    Nova forma de usar as transicoes

--- automatos/java/automata/src/main/java/br/com/pucgo/automata/impl/DFAImpl.java
+++ automatos/java/automata/src/main/java/br/com/pucgo/automata/impl/DFAImpl.java
@@ -8,23 +8,17 @@ import java.util.*;
 
 public class DFAImpl implements DFA {
 
-    public List<Character> symbols = new ArrayList<Character>();
-    public Map<State, Map<Character, List<State>>> transitions = new HashMap<State, Map<Character, List<State>>>();
-    public State startState = null;
 
+    //Lista de estados
+    List<MState> states = new ArrayList<MState>();
+
+    //Simbolos do Alfabeto
+    List<Character> symbols = new ArrayList<Character>();
+
+    private MState startState = null;
+
+    //Epsilon
     public static char Epsilon = '&';
-
-    /*
-    {
-        q1:{
-            '0':[q1,q2,q3],
-            '1':[q1,q2,q3]
-        },
-        q2:{
-            '0':[q1,q2,q3],
-            '1':[q1,q2,q3]
-        }
-    }*/
 
     public DFA minimize() {
         //TODO: implementar algoritmo de minimizacao de DFA
@@ -42,82 +36,65 @@ public class DFAImpl implements DFA {
     }
 
     public State addState(String stateName) {
-        State s = new State(stateName);
-        this.transitions.put(s, new HashMap<Character, List<State>>());
-        return s;
+        MState state = new MState(stateName, false);
+        this.states.add(state);
+        return state;
     }
 
     public State addState(String stateName, boolean isFinal) {
-        State s = new State(stateName, isFinal);
-        this.transitions.put(s, new HashMap<Character, List<State>>());
-        return s;
+        MState state = new MState(stateName, isFinal);
+        this.states.add(state);
+        return state;
     }
 
     public void setStart(State state) {
-        this.startState = state;
+        this.startState = (MState) state;
     }
 
     public void addTransition(State sourceState, char symbol, State... targetStates) {
-
-        //verifica se symbol de transicao existe no alfabeto
-        if(!symbols.contains(symbol)){
-            System.out.println("Simbolo de transicao nao existe no alfabeto!");
-            System.exit(1);
-        }
-
-        //Verifica se state informado existe
-        if(!transitions.containsKey(sourceState)){
-            System.out.println("SourceState Invalido!");
-            System.exit(1);
-        }
-
-        //verifica se targetState informado existe
-        if(!transitions.containsKey(targetStates[0])){
-            System.out.println("TargetState Invalido!");
-            System.exit(1);
-        }
-
-        //Busca a lista de targets para symbol a partir de sourceState
-        List<State> targets = transitions.get(sourceState).get(symbol);
-        if(targets == null){
-            //Cria uma lista de targets e os adiciona para symbol
-            transitions.get(sourceState).put(symbol, Arrays.asList(targetStates));
-            return;
-        }
-
-        //Caso targets ja existir verifica se o targetState ja existe na lista
-        if(targets.indexOf(targetStates[0]) != -1) {
-            System.out.println("TargetState ja esta na lista de targets!");
-            return;
-        }
-
-        //Adiciona na lista caso a lista ja exista e nao contenha targetStates
-        targets.add(targetStates[0]);
+        MState state = states.get(states.indexOf(sourceState));
+        state.getTransitions().put(symbol, (MState) targetStates[0]);
     }
-
 
     public boolean accept(String word) {
-        State currentState = this.startState;
-        State nextState = null;
+        MState currState = this.startState;
 
         char[] _word = word.toCharArray();
-        for(char symbol: _word){
+        for(Character w:_word){
 
-            //Alfabeto nao possui simbolo da palavra
-            if(!this.symbols.contains(symbol)) {
-                System.out.println("Alfabeto nao possui simbolo " + symbol + " da palavra");
+            /* Verifica se existe transicao para esse simbolo */
+            if(!currState.getTransitions().containsKey(w))
                 return false;
-            }
-            nextState = transitions.get(currentState).get(symbol).get(0);
 
-            //Verifica se existe transicao para este simbolo
-            if(nextState == null){
-                return false;
-            }
+            currState = currState.getTransitions().get(w);
 
-            currentState = nextState;
         }
 
-        return currentState.isFinal();
+        return currState.isFinal();
     }
+
+
+
+    private class MState extends State{
+
+        private Map<Character, MState> transitions = new HashMap<Character, MState>();
+
+        public MState(String name) {
+            super(name);
+        }
+
+        public MState(String name, boolean isFinal) {
+            super(name, isFinal);
+        }
+
+        public void addTransition(Character symbol, MState state){
+            this.transitions.put(symbol, state);
+        }
+
+        public Map<Character, MState> getTransitions(){
+            return this.transitions;
+        }
+
+    }
+
 }
